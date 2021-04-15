1219e7ebbcd412d14ebe03aaa34e4fb38af78503
amartin58 <adee1001@outlook.com>
2021-04-15 15:12:52 -0700

    First solution output

---
+++ birthday_checker.js
@@ -0,0 +1,16 @@
+// Solution output:
+// Not Birthday:
+
+// What is your name: Mike Jones 
+// What is your birth month: 2 
+// What is your birth day: 12 
+// What is your birth year: 1945 
+// Hi Mike Jones. Your birthday is 2-12-1945.
+
+const name = 'Mike Jones';
+const birthMonth = 2;
+const birthDay = 12;
+const birthYear = '1945';
+
+console.log(`Hi ${name}. Your birthday is ${birthMonth}-${birthDay}-${birthYear}.`)
+
