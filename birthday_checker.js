3e5fd11822ac81b844dd3d8f22e049d1244a762c
amartin58 <adee1001@outlook.com>
2021-04-16 16:21:33 -0700

    Update variable name to fullName

--- birthday_checker.js
+++ birthday_checker.js
@@ -1,15 +1,15 @@
 
-const name = 'Mike Jones'
+const fullName = 'Mike Jones'
 const birthMonth = 2;
 const birthDay = 12;
-const birthYear = '1990'
+const birthYear = '1945'
 
 
 const getUserBirthDay = function(birthMonth, birthDay, birthYear){
     if(birthMonth === 2 && birthDay === 4 && birthYear === '1990'){
         return 'Happy Birthday!'
-} else {
-    return `Hi ${name}. Your birthday is ${birthMonth}-${birthDay}-${birthYear}.`
+}   else {
+    return `Hi ${fullName}. Your birthday is ${birthMonth}-${birthDay}-${birthYear}.`
 }
 }
 console.log(getUserBirthDay(birthMonth, birthDay, birthYear))
