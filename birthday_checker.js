90789983edbb97f83de5d1e05f8b66065623d110
amartin58 <adee1001@outlook.com>
2021-04-16 16:06:13 -0700

    Remove name input from the if statement

--- birthday_checker.js
+++ birthday_checker.js
@@ -4,12 +4,13 @@ const birthMonth = 2;
 const birthDay = 12;
 const birthYear = '1945'
 
+
 const getUserBirthDay = function(name, birthMonth, birthDay, birthYear){
-    if(name == name && birthMonth === 2 && birthDay === 4 && birthYear === '1990'){
+    if(birthMonth === 2 && birthDay === 4 && birthYear === '1990'){
         return 'Happy Birthday!'
 } else {
     return `Hi ${name}. Your birthday is ${birthMonth}-${birthDay}-${birthYear}.`
 }
 }
-
 console.log(getUserBirthDay(name, birthMonth, birthDay, birthYear))
+
