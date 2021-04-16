552973f6a63999dcdbd2541625c96aed7487e3ed
amartin58 <adee1001@outlook.com>
2021-04-16 10:54:57 -0700

    Create birthday script

--- birthday_checker.js
+++ birthday_checker.js
@@ -1,25 +1,15 @@
 
-// const name = 'Mike Jones';
-// const birthMonth = 2;
-// const birthDay = 12;
-// const birthYear = '1945';
+const name = 'Mike Jones'
+const birthMonth = 2;
+const birthDay = 12;
+const birthYear = '1945'
 
-// if (birthYear === '1950'){
-//     console.log('Congratultions')
-// } else { 
-//     console.log('Not yet!')
-// }
-
-// console.log(`Hi ${name}. Your birthday is ${birthMonth}-${birthDay}-${birthYear}.`)
-
-
-const name = 'John Smith';
-const birthMonth = 4;
-const birthDay = 11;
-const birthYear = '1990';
-
-if (birthYear === '1990') {   
-
-console.log(`Hi ${name}. Your birthday is today: ${birthMonth}-${birthDay}-${birthYear}.`)
-console.log('Congratulations! You have become one year wiser!')
+const getUserBirthDay = function(name, birthMonth, birthDay, birthYear){
+    if(name == name && birthMonth === 2 && birthDay === 4 && birthYear === '1990'){
+        return 'Happy Birthday!'
+} else {
+    return `Hi ${name}. Your birthday is ${birthMonth}-${birthDay}-${birthYear}.`
 }
+}
+
+console.log(getUserBirthDay(name, birthMonth, birthDay, birthYear))
